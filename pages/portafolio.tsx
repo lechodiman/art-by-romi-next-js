399e7d02627aa0b855f728e1835c0391118383a4
Luis Chodiman <lechodiman@uc.cl>
2023-08-14 15:56:02 -0400

    feat: update portfolio

--- pages/portafolio.tsx
+++ pages/portafolio.tsx
@@ -1,26 +1,38 @@
 import { TypographyH1 } from '@/components/TypographyH1';
 import Image from 'next/image';
 import bruno from '@/public/images/bruno.jpg';
+import chola from '@/public/images/chola.jpg';
 import copito from '@/public/images/copito.jpg';
 import domi from '@/public/images/domi.jpg';
+import domi2 from '@/public/images/domi-2.jpg';
 import lana from '@/public/images/lana.jpg';
+import luna2 from '@/public/images/luna-2.jpg';
+import locky2 from '@/public/images/locky-2.jpg';
 import pajarraco from '@/public/images/pajarraco.jpg';
+import pia2 from '@/public/images/pia-2.jpg';
+import popom from '@/public/images/popom.jpg';
 import ruffo from '@/public/images/ruffo.jpg';
 import thor from '@/public/images/thor.jpg';
 import tomi from '@/public/images/tomi.jpg';
 
-export default function Portafolio() {
-  const images = [
-    { src: tomi, alt: 'Image 12' },
-    { src: bruno, alt: 'Image 1' },
-    { src: copito, alt: 'Image 2' },
-    { src: ruffo, alt: 'Image 10' },
-    { src: lana, alt: 'Image 4' },
-    { src: pajarraco, alt: 'Image 8' },
-    { src: domi, alt: 'Image 3' },
-    { src: thor, alt: 'Image 11' },
-  ];
+const images = [
+  { src: bruno, alt: 'Bruno' },
+  { src: chola, alt: 'Chola' },
+  { src: copito, alt: 'Copito' },
+  { src: domi, alt: 'Domi' },
+  { src: domi2, alt: 'Domi 2' },
+  { src: lana, alt: 'Lana' },
+  { src: luna2, alt: 'Luna 2' },
+  { src: locky2, alt: 'Locky 2' },
+  { src: pajarraco, alt: 'Pajarraco' },
+  { src: pia2, alt: 'Pia 2' },
+  { src: popom, alt: 'Popom' },
+  { src: ruffo, alt: 'Ruffo' },
+  { src: thor, alt: 'Thor' },
+  { src: tomi, alt: 'Tomi' },
+];
 
+export default function Portafolio() {
   return (
     <div className='bg-gray-200'>
       <section className='container px-5 mx-auto space-y-8 text-center py-14'>
