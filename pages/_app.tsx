1114f6fb06bf7ac63db3bdbda981d562de9da8fc
Romina Rivera <rdrivera@uc.cl>
2025-04-04 23:35:11 -0300

    Agregar etiquetas Open Graph para mejorar la integración en redes sociales

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -28,6 +28,27 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name='googlebot' content='index, follow' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <meta charSet='utf-8' />
+        {/* Open Graph */}
+        <meta property='og:site_name' content='Retratos Romi' />
+        <meta property='og:locale' content='es_CL' />
+        <meta
+          property='og:title'
+          content='Retratos Romi - Retratos Personalizados de Mascotas'
+        />
+        <meta
+          property='og:description'
+          content='Convierte a tu mascota en una obra de arte con Retratos Romi. Pinturas al óleo personalizadas, hechas con amor.'
+        />
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content='https://www.retratosromi.com/' />
+        <meta
+          property='og:image'
+          content='https://www.retratosromi.com/images/portada.jpg'
+        />
+        <meta property='og:image:alt' content='Retrato al óleo de mascota' />
+        <meta property='og:image:type' content='image/jpeg' />
+        <meta property='og:image:width' content='1200' />
+        <meta property='og:image:height' content='630' />
       </Head>
       <Layout>
         <Component {...pageProps} />
