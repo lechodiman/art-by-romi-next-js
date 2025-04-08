3ab926c552ee36ab32949c0a6fea3e08c47a8dd5
Romina Rivera Cáceres <66977787+romirivera@users.noreply.github.com>
2025-04-07 23:41:23 -0400

    add seo tags (#11)
    * Actualizar título y agregar meta descripción para mejorar SEO
    
    * Agregar meta etiqueta de keywords para mejorar SEO
    
    * Agregar meta etiquetas para mejorar SEO y optimizar la visualización en dispositivos móviles
    
    * Agregar etiquetas Open Graph para mejorar la integración en redes sociales
    
    * Agregar etiquetas de Twitter Card para mejorar la integración en redes sociales
    
    * Eliminar imágenes no utilizadas para optimizar el proyecto y agregar imagen de portada para seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,7 +15,56 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={`${playfair.variable}`}>
       <Head>
-        <title>Retratos Romi</title>
+        <title>Retratos Romi - Retratos Personalizados de Mascotas</title>
+        <meta
+          name='description'
+          content='Retratos óleo personalizados de tus mascotas, con un estilo único y realista. Captura la esencia de tu amigo peludo en una obra de arte.'
+        />
+        <meta
+          name='keywords'
+          content='retratos de mascotas, arte personalizado, pintura al óleo, mascotas, arte, cuadro de mascotas, arte, mascotas, óleo, retrata a tu mascota, pinto a tu mascota.'
+        />
+        <meta name='robots' content='index, follow' />
+        <meta name='googlebot' content='index, follow' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <meta charSet='utf-8' />
+        {/* Open Graph */}
+        <meta property='og:site_name' content='Retratos Romi' />
+        <meta property='og:locale' content='es_CL' />
+        <meta
+          property='og:title'
+          content='Retratos Romi - Retratos Personalizados de Mascotas'
+        />
+        <meta
+          property='og:description'
+          content='Convierte a tu mascota en una obra de arte con Retratos Romi. Pinturas al óleo personalizadas, hechas con amor.'
+        />
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content='https://www.retratosromi.com/' />
+        <meta
+          property='og:image'
+          content='https://www.retratosromi.com/images/portada.jpg'
+        />
+        <meta property='og:image:alt' content='Retrato al óleo de mascota' />
+        <meta property='og:image:type' content='image/jpeg' />
+        <meta property='og:image:width' content='1200' />
+        <meta property='og:image:height' content='630' />
+        {/* twitter card*/}
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:site' content='@tu_usuario' />
+        <meta name='twitter:creator' content='@tu_usuario' />
+        <meta
+          name='twitter:title'
+          content='Retratos Romi - Retratos Personalizados de Mascotas'
+        />
+        <meta
+          name='twitter:description'
+          content='Retratos al óleo personalizadas de mascotas. Un regalo único y especial para los amantes de los animales.'
+        />
+        <meta
+          name='twitter:image'
+          content='https://www.retratosromi.com/imagenes/portada.jpg'
+        />
       </Head>
       <Layout>
         <Component {...pageProps} />
