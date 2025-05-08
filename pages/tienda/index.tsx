ab0f9db8717f41cd6219d89d39afdb6b5ea5b706
Romina Rivera <rdrivera@uc.cl>
2025-05-08 01:32:07 -0400

    Actualizar dummyProducts en Tienda para usar _id en lugar de id y agregar productos adicionales

--- pages/tienda/index.tsx
+++ pages/tienda/index.tsx
@@ -4,14 +4,29 @@ import { Product } from '@/types/Product';
 
 const dummyProducts: Product[] = [
   {
-    id: '1',
+    _id: '1',
     name: 'Retrato Mini',
     description: 'Retrato personalizado, óleo sobre tela tamaño Mini 20cm x 25cm',
     price: 40000,
     images: ['/images/product1.jpg'],
     category: 'retratos',
   },
-  // Agrega más productos aquí
+  {
+    _id: '2',
+    name: 'Retrato Mediano',
+    description: 'Retrato personalizado, óleo sobre tela tamaño Mediano 25cm x 30cm',
+    price: 45000,
+    images: ['/images/product2.jpg'],
+    category: 'retratos',
+  },
+  {
+    _id: '3',
+    name: 'Retrato Grande',
+    description: 'Retrato personalizado, óleo sobre tela tamaño Grande 30cm x 40cm',
+    price: 50000,
+    images: ['/images/product3.jpg'],
+    category: 'retratos',
+  },
 ];
 
 export default function Tienda() {
